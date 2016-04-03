919822a5eeb8499e004c3b5a880cda27d5e0c720
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-04-03 18:49:37 +0200

    rake routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
   get 'users/index'
-
   get 'users/new'
-
   get 'users/create'
-
   get 'users/edit'
-
   get 'users/update'
-
   get 'users/show'
-
   get 'users/destroy'
-
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
