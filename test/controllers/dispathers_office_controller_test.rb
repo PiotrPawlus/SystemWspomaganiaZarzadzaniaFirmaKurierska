f20bb31ee18b11e7ca88fd91abb8ee26b87cdb49
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-04-03 20:13:08 +0200

    dispathers vs dipsathers part2

--- test/controllers/dispathers_office_controller_test.rb
+++ test/controllers/dispathers_office_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class DipsathersOfficeControllerTest < ActionController::TestCase
+class DispathersOfficeControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
