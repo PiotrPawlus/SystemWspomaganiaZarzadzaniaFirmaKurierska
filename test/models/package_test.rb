b97b0f6058fe25771869d0f5cb5fcc5838d0e74b
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-04-05 19:57:33 +0200

    add first test

--- test/models/package_test.rb
+++ test/models/package_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PackageTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "the_count_of_packages" do
+    package = create(:package)
+    assert_equal(Package.count, 1)
   end
 end
