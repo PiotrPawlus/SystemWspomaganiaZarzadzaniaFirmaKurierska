2965bdc2c11eba33528a6bd6b423030ddaf698a4
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-04-05 19:29:34 +0200

    comment tests, prepare for testing

--- test/factories.rb
+++ test/factories.rb
@@ -1,5 +1,13 @@
- FacotryGirl.define do
-   factory :user do
-     
+ FactoryGirl.define do
+   factory :package do
+     weight           2.0
+     size_X           0.5
+     size_Y           0.5
+     size_Z           0.5
+     delivery_address "Warszawaska 24, Kraków"
+     sender_address   "Warszawaska 23, Kraków"
+     actual_place     "Magazyn główny - Pędzichów"
+     phone            "600700800"
+     cost             17.99
    end
  end
