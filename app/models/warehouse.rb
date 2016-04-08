b2cc79a745cfe52835bc04996aeeb73c91d9cf90
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-04-08 23:35:31 +0200

    fix validation

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -2,13 +2,26 @@ class Warehouse < ActiveRecord::Base
   has_many :orders, :dependent=> :destroy
   has_many :users, :dependent=> :destroy
 
-  validates :name, :address, :capacity, presence: true
+  validates :name, :city, :street, :house_number, :postcode, :capacity, presence: true
 
   validates :name,
           length: { minimum: 1, maximum: 50, :message => "Nazwa magazynu musi zawierać od 1 do 50 znaków."}
 
-  validates :address,
-          length: { minimum: 1, maximum: 100, :message => "Adres magazynu musi zawierać od 1 do 100 znaków."}
+  validates :city,
+          length: { minimum: 1, maximum: 50, :message => "Adres magazynu musi zawierać od 1 do 50 znaków."}
+
+
+  validates :street,
+          length: { minimum: 1, maximum: 50, :message => "Ulica dla magazynu musi zawierać od 1 do 50 znaków."}
+
+  validates :house_number,
+          length: { minimum: 1, maximum: 10, :message => "Numer domu dla magazynu musi zawierać od 1 do 10 znaków."}
+
+  validates :local_nuber,
+          length: { minimum: 1, maximum: 10, :message => "Numer lokalu dla magazynu musi zawierać od 1 do 10 znaków."}
+
+  validates :postcode,
+          length: { minimum: 1, maximum: 10, :message => "Numer domu dla magazynu musi zawierać od 1 do 10 znaków."}
 
   validates :capacity,
           numericality: { only_integer: true }
