6ec4778ce788bf4d294317ab718cbc6bca899129
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-04-10 20:47:17 +0200

    prepare controller tests

--- test/controllers/couriers_controller_test.rb
+++ test/controllers/couriers_controller_test.rb
@@ -1,39 +1,34 @@
 require 'test_helper'
 
 class CouriersControllerTest < ActionController::TestCase
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  # end
-  #
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
-  #
-  # test "should get create" do
-  #   get :create
-  #   assert_response :success
-  # end
-  #
-  # test "should get edit" do
-  #   get :edit
-  #   assert_response :success
-  # end
-  #
-  # test "should get update" do
-  #   get :update
-  #   assert_response :success
-  # end
-  #
-  # test "should get show" do
-  #   get :show
-  #   assert_response :success
-  # end
-  #
-  # test "should get destroy" do
-  #   get :destroy
-  #   assert_response :success
-  # end
+
+  test "should get index" do
+    get :index
+    assert_response :success, "Cannot get a index"
+    assert_not_nil assigns(:couriers)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success, "Cannot get a new"
+  end
+
+  test "should get edit" do
+    courier = create(:courier)
+    get( :edit, {'id' => courier.id})
+    assert_response :success, "Cannot get an edit"
+  end
+
+  test "should get show" do
+    courier = create(:courier)
+    get( :show, {'id' => courier.id})
+    assert_response :success, "Cannot get a show"
+  end
+
+  test "should get destroy" do
+    courier = create(:courier)
+    get( :destroy, {'id' => courier.id})
+    assert_response :success, "Cannot get destroy"
+  end
 
 end
