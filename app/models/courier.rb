1211820cff48834feafd580decefa96e472fb59d
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-04-10 21:14:57 +0200

    max characters from 100 to 250

--- app/models/courier.rb
+++ app/models/courier.rb
@@ -4,5 +4,5 @@ class Courier < ActiveRecord::Base
 
   validates :precinct,
             presence: true,
-            length: { minimum: 5, maximum: 100, :message => "Obręb dla kuriera musi zawierać od 5 do 100 znaków."}
+            length: { minimum: 5, maximum: 250, :message => "Obręb dla kuriera musi zawierać od 5 do 250 znaków."}
 end
