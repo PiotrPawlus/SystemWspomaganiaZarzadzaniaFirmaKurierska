24ee5a6bdbaeebb695bbbf9101489b65d9d4e4f1
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-04-10 21:14:20 +0200

    prepare new and create func for new courier

--- app/controllers/couriers_controller.rb
+++ app/controllers/couriers_controller.rb
@@ -4,9 +4,16 @@ class CouriersController < ApplicationController
   end
 
   def new
+    @courier = Courier.new
   end
 
   def create
+    @new_courier = Courier.new(courier_parameters)
+    if @new_courier.save
+      redirect_to(:controller => 'couriers', :action => index)
+    else
+      render('new')
+    end
   end
 
   def edit
@@ -20,4 +27,9 @@ class CouriersController < ApplicationController
 
   def destroy
   end
+
+  def courier_parameters
+    params.require(:courier).permit(:precinct)
+  end
+
 end
