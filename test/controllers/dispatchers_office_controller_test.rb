cda7e33c85ef8bb6206683930e014d60a60d4b22
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-04-12 22:31:21 +0200

    Prepare test for dispatcher office controller - Failure

--- test/controllers/dispatchers_office_controller_test.rb
+++ test/controllers/dispatchers_office_controller_test.rb
@@ -1,39 +1,33 @@
 require 'test_helper'
 
 class DispatchersOfficeControllerTest < ActionController::TestCase
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  # end
-  #
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
-  #
-  # test "should get create" do
-  #   get :create
-  #   assert_response :success
-  # end
-  #
-  # test "should get edit" do
-  #   get :edit
-  #   assert_response :success
-  # end
-  #
-  # test "should get update" do
-  #   get :update
-  #   assert_response :success
-  # end
-  #
-  # test "should get show" do
-  #   get :show
-  #   assert_response :success
-  # end
-  #
-  # test "should get destroy" do
-  #   get :destroy
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:dispatcher_offices)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should get edit" do
+    dispatcher_office = create(:dispatcher_office)
+    get(:edit, {"id" => dispatcher_office.id})
+    assert_response :success
+  end
+
+  test "should get show" do
+    dispatcher_office = create(:dispatcher_office)
+    get(:show, {'id' => dispatcher_office.id})
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    dispatcher_office = create(:dispatcher_office)
+    get(:destroy, {'id' => dispatcher_office.id})
+    assert_response :success
+  end
 
 end
