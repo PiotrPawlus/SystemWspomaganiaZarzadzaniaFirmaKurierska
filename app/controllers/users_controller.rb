b5d5e311fb2db9f6b11d67d91fcd033171f094c6
andrzej0221 <andrzejo94@gmail.com>
2016-04-12 22:47:04 +0200

    add content to user controller/ show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,10 +26,11 @@ class UsersController < ApplicationController
       redirect_to(:action => 'index')
     else
       render('edit')
-    end 
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def destroy
