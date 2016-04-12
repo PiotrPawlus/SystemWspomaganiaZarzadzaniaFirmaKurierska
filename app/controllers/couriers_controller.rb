e495f7605bff9a8b1401083908417f8e4affeddc
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-04-12 12:37:13 +0200

    made edit courier data

--- app/controllers/couriers_controller.rb
+++ app/controllers/couriers_controller.rb
@@ -10,16 +10,23 @@ class CouriersController < ApplicationController
   def create
     @new_courier = Courier.new(courier_parameters)
     if @new_courier.save
-      redirect_to(:controller => 'couriers', :action => index)
+      redirect_to(:controller => 'couriers', :action=>'index')
     else
       render('new')
     end
   end
 
   def edit
+    @courier = Courier.find(params[:id])
   end
 
   def update
+    @courier = Courier.find(params[:id])
+    if @courier.update_attributes(courier_parameters)
+      redirect_to(:action=>'index')
+    else
+      render('edit')
+    end
   end
 
   def show
