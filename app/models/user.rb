36033101a2932156f0300ffb87092152a5fbc176
andrzej0221 <andrzejo94@gmail.com>
2016-04-14 23:43:22 +0200

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ActiveRecord::Base
   belongs_to :warehouse
   belongs_to :dispatcher_office
   belongs_to :courier
+
+  validates :email, :password, :password_confirmation, :name, :surname, :birth_date, :position, presence: true
+
+
+  validates :email,
+          length: { minimum: 1, maximum: 50, :message => "Email musi zawierać od 1 do 50 znaków."}
+
+  validates :password,
+          length: { minimum: 1, maximum: 50, :message => "Hasło musi zawierać od 1 do 50 znaków."}
+
+  validates :name,
+          length: { minimum: 1, maximum: 50, :message => "Imię musi zawierać od 1 do 50 znaków."}
+
+  validates :surname,
+          length: { minimum: 1, maximum: 50, :message => "Nazwisko musi zawierać od 1 do 50 znaków."}
+
+
 end
