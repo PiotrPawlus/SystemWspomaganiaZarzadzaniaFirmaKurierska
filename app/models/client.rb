e0f75a88d1a4a20cab0c3f8494b9df14538c9226
andrzej0221 <andrzejo94@gmail.com>
2016-04-15 01:53:14 +0200

    add gemfile Phonelib to validate phone

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,29 @@
 class Client < ActiveRecord::Base
   has_secure_password
   has_many :orders, :dependent=> :destroy
+
+  validates :email, :password, :password_confirmation, :name, :surname, :phone, presence: true
+
+
+  validates :email,
+          length: { minimum: 1, maximum: 50, :message => "Email musi zawierać od 1 do 50 znaków."}
+
+  validates :password,
+          length: { minimum: 1, maximum: 50, :message => "Hasło musi zawierać od 1 do 50 znaków."}
+
+  validates :name,
+          length: { minimum: 1, maximum: 50, :message => "Imię musi zawierać od 1 do 50 znaków."}
+
+  validates :surname,
+          length: { minimum: 1, maximum: 50, :message => "Nazwisko musi zawierać od 1 do 50 znaków."}
+
+  validates :phone,
+          phone: {possible: true, :message => "Zły numer telefonu"}
+
+
+
+
+
+
+
 end
