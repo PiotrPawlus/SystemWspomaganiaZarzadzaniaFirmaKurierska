e368e5487cfab6be41e00e765595fad3f40d2b98
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-04-15 22:07:35 +0200

    Packages - write test for controllers

--- test/controllers/packages_controller_test.rb
+++ test/controllers/packages_controller_test.rb
@@ -1,39 +1,32 @@
 require 'test_helper'
 
 class PackagesControllerTest < ActionController::TestCase
-#   test "should get index" do
-#     get :index
-#     assert_response :success
-#   end
-#
-#   test "should get new" do
-#     get :new
-#     assert_response :success
-#   end
-#
-#   test "should get create" do
-#     get :create
-#     assert_response :success
-#   end
-#
-#   test "should get edit" do
-#     get :edit
-#     assert_response :success
-#   end
-#
-#   test "should get update" do
-#     get :update
-#     assert_response :success
-#   end
-#
-#   test "should get show" do
-#     get :show
-#     assert_response :success
-#   end
-#
-#   test "should get destroy" do
-#     get :destroy
-#     assert_response :success
-#   end
-#
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:packages)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should get edit" do
+    package = create(:package)
+    get(:edit, {'id' => package.id})
+    assert_response :success
+  end
+
+  test "should get show" do
+    package = create(:package)
+    get(:show, {'id' => package.id})
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    package = create(:package)
+    get(:destroy, {'id' => package.id})
+    assert_response :success
+  end
 end
