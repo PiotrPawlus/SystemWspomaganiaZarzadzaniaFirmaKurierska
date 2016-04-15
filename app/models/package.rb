67d0167343c877311ba54e513558279fdc645c5d
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-04-15 21:58:29 +0200

    Packages - add validatios of name and surname of recipient

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,8 +1,10 @@
 class Package < ActiveRecord::Base
   belongs_to :order
 
-  validates :weight, :size_X, :size_Y, :size_Z, :city_delivery, :street_delivery, :house_number_delivery, :local_number_delivery, :postcode_delivery,
-   :city_sender, :street_sender, :house_number_sender, :local_number_sender, :postcode_sender, :actual_place, :phone_delivery, :phone_sender, :cost, presence: true
+  validates :weight, :size_X, :size_Y, :size_Z,
+    :name_of_recipient, :surname_of_recipient, :city_delivery, :street_delivery, :house_number_delivery, :local_number_delivery, :postcode_delivery,
+    :city_sender, :street_sender, :house_number_sender, :local_number_sender, :postcode_sender, :actual_place, :phone_delivery, :phone_sender, :cost,
+    presence: true
 
   validates :weight,
           numericality: { only_float: true }
@@ -16,9 +18,14 @@ class Package < ActiveRecord::Base
   validates :size_Z,
           numericality: { only_float: true }
 
+  validates :name_of_recipient,
+          length: { minimum: 1, maximum: 50, :message => "Imie adresata musi zawierać od 1 do 50 znaków."}
+
+  validates :surname_of_recipient,
+          length: { minimum: 1, maximum: 50, :message => "Nazwisko adresata musi zawierać od 1 do 50 znaków."}
+
   validates :city_delivery,
           length: { minimum: 1, maximum: 50, :message => "Adres magazynu musi zawierać od 1 do 50 znaków."}
-
 
   validates :street_delivery,
           length: { minimum: 1, maximum: 50, :message => "Ulica dla magazynu musi zawierać od 1 do 50 znaków."}
