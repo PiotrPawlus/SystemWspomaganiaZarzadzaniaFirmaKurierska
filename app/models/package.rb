575f4b62599e903a4d31dcd856fccdbeb7fe1ad7
andrzej0221 <andrzejo94@gmail.com>
2016-04-15 19:04:00 +0200

    Validates package

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,3 +1,24 @@
 class Package < ActiveRecord::Base
   belongs_to :order
+
+  validates :weight, :size_X, :size_Y, :size_Z, :delivery_address, :sender_address, :actual_place, :phone, :cost, presence: true
+
+  validates :weight,
+          numericality: { only_float: true }
+
+  validates :size_X,
+          numericality: { only_float: true }
+
+  validates :size_Y,
+          numericality: { only_float: true }
+
+  validates :size_Z,
+          numericality: { only_float: true }
+
+  validates :delivery_address,
+          length: { minimum: 1, maximum: 150, :message => "Adres musi zawierać maksymalnie 150 znaków."}
+
+
+
+
 end
