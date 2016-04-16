8c98ce9e1399d86064de79c09db1eb5d38ebd295
andrzej0221 <andrzejo94@gmail.com>
2016-04-16 17:34:39 +0200

    validations in orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,5 +5,14 @@ class Order < ActiveRecord::Base
   belongs_to :courier
   belongs_to :client
 
+  validates :order_number, :status, :warehouse_id, :dispatcher_office_id, :courier_id, :client_id, presence: true
+
+  validates :order_number,
+          numericality: { only_integer: true }
+
+  validates :status,
+          length: { minimum: 1, maximum: 80, :message => "Status musi zawierać od 1 do 80 znaków."}
+
+
 
 end
