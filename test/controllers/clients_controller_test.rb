789ac3c87cd29ff290b20aef878c36a015697eb8
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-04-18 15:45:44 +0200

    add tests for Clients, Home, Orders, Users controller

--- test/controllers/clients_controller_test.rb
+++ test/controllers/clients_controller_test.rb
@@ -1,39 +1,33 @@
 require 'test_helper'
 
 class ClientsControllerTest < ActionController::TestCase
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  # end
-  #
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
-  #
-  # test "should get create" do
-  #   get :create
-  #   assert_response :success
-  # end
-  #
-  # test "should get edit" do
-  #   get :edit
-  #   assert_response :success
-  # end
-  #
-  # test "should get update" do
-  #   get :update
-  #   assert_response :success
-  # end
-  #
-  # test "should get show" do
-  #   get :show
-  #   assert_response :success
-  # end
-  #
-  # test "should get destroy" do
-  #   get :destroy
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:clients)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should get edit" do
+    client = create(:client)
+    get(:edit, {'id' => client.id})
+    assert_response :success
+  end
+
+  test "should get show" do
+    client = create(:client)
+    get(:show, {'id' => client.id})
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    client = create(:client)
+    get(:destroy, {'id' => client.id})
+    assert_response :success
+  end
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,39 +1,33 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-#   test "should get index" do
-#     get :index
-#     assert_response :success
-#   end
-#
-#   test "should get new" do
-#     get :new
-#     assert_response :success
-#   end
-#
-#   test "should get create" do
-#     get :create
-#     assert_response :success
-#   end
-#
-#   test "should get edit" do
-#     get :edit
-#     assert_response :success
-#   end
-#
-#   test "should get update" do
-#     get :update
-#     assert_response :success
-#   end
-#
-#   test "should get show" do
-#     get :show
-#     assert_response :success
-#   end
-#
-#   test "should get destroy" do
-#     get :destroy
-#     assert_response :success
-#   end
-#
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:users)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should get edit" do
+    user = create(:user)
+    get(:edit, {'id' => user.id})
+    assert_response :success
+  end
+
+  test "should get show" do
+    user = create(:user)
+    get(:show, {'id' => user.id})
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    user = create(:user)
+    get(:destroy, {'id' => user.id})
+    assert_response :success
+  end
+
 end
