ec9e62c60c2ca5c1b6c0ee2e0fe0db522c29805b
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-04-23 13:03:03 +0200

    generate new schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160423101431) do
+ActiveRecord::Schema.define(version: 20160423105932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "clients", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                              default: "", null: false
+    t.string   "encrypted_password",                 default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "name",                   limit: 80,               null: false
+    t.string   "surname",                limit: 100,              null: false
+    t.string   "phone",                  limit: 15,               null: false
   end
 
   add_index "clients", ["email"], name: "index_clients_on_email", unique: true, using: :btree
@@ -91,18 +94,26 @@ ActiveRecord::Schema.define(version: 20160423101431) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                              default: "",    null: false
+    t.string   "encrypted_password",                 default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
+    t.string   "name",                   limit: 50,                  null: false
+    t.string   "surname",                limit: 50,                  null: false
+    t.date     "birth_date",                                         null: false
+    t.string   "position",               limit: 100,                 null: false
+    t.boolean  "is_admin",                           default: false, null: false
+    t.boolean  "is_dispather"
+    t.boolean  "is_courier"
+    t.boolean  "is_warehouse"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
