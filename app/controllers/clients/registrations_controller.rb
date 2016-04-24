6945efad8dedca4b6ebe42bc91b6c733a3c60293
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-04-24 23:30:13 +0200

    add possibility to login for client

--- app/controllers/clients/registrations_controller.rb
+++ app/controllers/clients/registrations_controller.rb
@@ -1,11 +1,11 @@
 class Clients::RegistrationsController < Devise::RegistrationsController
-  # private
-  # def sign_up_params
-  #   params.require(:user).permit(:email, :password, :password_confirmation, :name, :surname, :birth_date, :position)
-  # end
-  #
-  # def account_update_params
-  #   params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :name, :surname, :birth_date, :position)
-  # end
+  private
+  def sign_up_params
+    params.require(:client).permit(:email, :password, :password_confirmation, :name, :surname, :phone)
+  end
+
+  def account_update_params
+    params.require(:client).permit(:email, :password, :password_confirmation, :current_password, :name, :surname, :phone)
+  end
 
 end
