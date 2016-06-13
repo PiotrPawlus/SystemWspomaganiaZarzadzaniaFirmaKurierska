d19cac74a4f7024007fdd94c44d0d7a685b0fd86
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-06-14 00:20:15 +0200

    give users permisions for diffrent accounts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,78 @@ class UsersController < ApplicationController
     redirect_to(:action => 'index')
   end
 
+  def updateAdmin
+    user = User.find(params[:id])
+
+    if user.is_admin
+      if user.update_attribute(:is_admin, false)
+        redirect_to(:action=>'index')
+      else
+        render('index')
+      end
+    else
+      if user.update_attribute(:is_admin, true)
+        redirect_to(:action=>'index')
+      else
+        render('index')
+      end
+    end
+  end
+
+  def updateDispatcher
+    user = User.find(params[:id])
+
+    if user.is_dispather
+      if user.update_attribute(:is_dispather, false)
+        redirect_to(:action=>'index')
+      else
+        render('index')
+      end
+    else
+      if user.update_attribute(:is_dispather, true)
+        redirect_to(:action=>'index')
+      else
+        render('index')
+      end
+    end
+  end
+
+  def updateCourier
+    user = User.find(params[:id])
+
+    if user.is_courier
+      if user.update_attribute(:is_courier, false)
+        redirect_to(:action=>'index')
+      else
+        render('index')
+      end
+    else
+      if user.update_attribute(:is_courier, true)
+        redirect_to(:action=>'index')
+      else
+        render('index')
+      end
+    end
+  end
+
+  def updateWarehouse
+    user = User.find(params[:id])
+
+    if user.is_warehouse
+      if user.update_attribute(:is_warehouse, false)
+        redirect_to(:action=>'index')
+      else
+        render('index')
+      end
+    else
+      if user.update_attribute(:is_warehouse, true)
+        redirect_to(:action=>'index')
+      else
+        render('index')
+      end
+    end
+  end
+
   def user_update_parameters
     params.require(:task).permit(:is_done, :title, :project_id)
   end
